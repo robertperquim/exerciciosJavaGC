6ce1b7ff9382588dfc6e50466e36b9ab42081250
robertperquim <perquimrobert@gmail.com>
2023-09-07 00:31:20 -0300

    Adicionando exercicio 09

---
+++ src/aula00/exercicio09/exercicio09.java
@@ -0,0 +1,29 @@
+/*Crie um programa em Java que peça ao usuário para inserir
+seu ano de nascimento e, em seguida, calcule e exiba a idade*/
+package aula00.exercicio09;
+
+import java.util.Scanner;
+
+public class exercicio09 {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int anoNascimento, anoReferencia = 2023;
+
+
+        do {
+
+            System.out.println("Informe seu ano de nascimento: ");
+            anoNascimento = input.nextInt();
+        } while (anoNascimento > anoReferencia);
+
+
+
+        System.out.println("Voce tem " + (anoReferencia - anoNascimento) + " anos!");
+
+
+
+
+    }
+}
